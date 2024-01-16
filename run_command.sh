12adb57750929535a249d8595ca8575590dd3502
Ehsan-aghapour <aghapour.ehsan17@gmail.com>
2024-01-16 20:02:36 +0100

    run_command change

--- run_command.sh
+++ run_command.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-adb shell /data/local/ARM-CO-UP/test_graph/graph_mobilenet_pipeline --data=/data/local/ARM-CO-UP/assets//mobilenet/ --image=/data/local/ARM-CO-UP/assets//ppm_images_224/ --labels=/data/local/ARM-CO-UP/assets//labels.txt --order=GGGGGGGGGGGGGG --kernel_c=96 --power_profile_mode=layers --n=10
+adb shell /data/local/ARM-CO-UP/test_graph/graph_yolov3_pipeline --data=/data/local/ARM-CO-UP/assets//yolov3/ --image=/data/local/ARM-CO-UP/assets//ppm_images_608/ --labels=/data/local/ARM-CO-UP/assets//coco.names --order=NBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBNBN --kernel_c=96 --power_profile_mode=layers --n=10
